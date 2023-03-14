d3ea77f8e8631295e889bf88e9bc0ecbc12aebb1
Joanna Szaban <joannaszaban@MacBook-Pro-Joanna.local>
2023-03-14 09:38:08 +0100

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,13 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Joanna Szaban on 21/08/2022.
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
